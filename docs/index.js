ead2a7f956b2e1da6cd6c25b16c5e0049564d195
poyo-poyo <ik0390@gmail.com>
2018-08-17 17:57:18 +0900

    update

--- docs/index.js
+++ docs/index.js
@@ -1,21 +1,3 @@
-// const init = async () => {
-//   return fetch("log.json")
-//     .then((response) => {
-//       return response.json()
-//     })
-//     .then((json) => {
-//       return json
-//     })
-// }
-
-// init()
-//   .then((res) => {
-//     console.log(res)
-//   })
-//   .catch((err) => {
-//     console.error(err)
-//   })
-
 fetch("log.json")
   .then((response) => {
     return response.json()
@@ -26,138 +8,148 @@ fetch("log.json")
 const run = ({ initial, final, log }) => {
   var { app, h } = hyperapp
   const n = 9, k = 3
-  const BASE_INTERVAL = 20
+  const BASE_INTERVAL = 800
 
   const state = {
     table: initial.map((row) => {
       return row.map((val) => {
-        return { val: val, textColor: "black", backgroundColor: "white" }
+        return { val: (val === 0 ? " " : val), textColor: "black", backgroundColor: "white" }
       })
     }),
+    log: log,
+    previousRecord: null,
+    intervalID: null,
+  }
+
+  const updateCell = (pos, val, color) => {
+    state.table[pos.r][pos.c].val = val
+    state.table[pos.r][pos.c].backgroundColor = color
+    state.table[pos.r][pos.c].fontStyle = "italic"
+  }
+
+  const changeRangeColor = (pos, color) => {
+    for (let l = 0; l < n; l++) {
+      state.table[pos.r][l].backgroundColor = color
+      state.table[l][pos.c].backgroundColor = color
+    }
+    const rr = Math.floor(pos.r / k), cc = Math.floor(pos.c / k)
+    for (let i = rr * k; i < (rr + 1) * k; i++) { // ブロック
+      for (let j = cc * k; j < (cc + 1) * k; j++) {
+        state.table[i][j].backgroundColor = color
+      }
+    }
+  }
+
+  const changeTargetCellColor = (pos, val, color) => {
+    for (let l = 0; l < n; l++) { // 行と列
+      if (state.table[pos.r][l].val === val) {
+        state.table[pos.r][l].backgroundColor = color
+      }
+      if (state.table[l][pos.c].val === val) {
+        state.table[l][pos.c].backgroundColor = color
+      }
+    }
+    const rr = Math.floor(pos.r / k), cc = Math.floor(pos.c / k)
+    for (let i = rr * k; i < (rr + 1) * k; i++) { // ブロック
+      for (let j = cc * k; j < (cc + 1) * k; j++) {
+        if (state.table[i][j].val === val) {
+          state.table[i][j].backgroundColor = color
+        }
+      }
+    }
   }
 
   const actions = {
-    updateCell: ({ pos, val }) => { // accepts a single argument
-      return ((state) => {
-        state.table[pos[0]][pos[1]].val = val
-        return ({ table: state.table }) // ???
+    changeInterval: (ev) => {
+      const r = parseInt(ev.target.value.replace("x", ""), 10)
+      return ((state, actions) => {
+        actions.set(BASE_INTERVAL / r)
       })
     },
-    changeTargetCellColor: ({ pos, val, color }) => { // valと同じ数字のセルをcolor色にする
-      return ((state) => {
-        for (let l = 0; l < n; l++) { // 行と列
-          if (l != pos[1] && state.table[pos[0]][l].val == val) {
-            state.table[pos[0]][l].backgroundColor = color
-          }
-          if (l != pos[0] && state.table[l][pos[1]].val == val) {
-            state.table[l][pos[1]].backgroundColor = color
-          }
+    set: (interval) => {
+      return ((state, actions) => {
+        if (state.intervalID !== null) {
+          clearInterval(state.intervalID)
         }
-        const m0 = Math.floor(pos[0] / k), m1 = Math.floor(pos[1] / k)
-        for (let i = m0 * k; i < (m0 + 1) * k; i++) { // ブロック
-          for (let j = m1 * k; j < (m1 + 1) * k; j++) {
-            if (i == pos[0] && j == pos[1]) continue;
-            if (state.table[i][j].val == val) state.table[i][j].backgroundColor = color
-          }
-        }
-        return ({ table: state.table })
+        state.intervalID = setInterval(actions.draw, interval)
       })
     },
-    changeBackgroundColor: ({ pos, color }) => {
-      return ((state) => {
-        for (let l = 0; l < n; l++) {
-          state.table[pos[0]][l].backgroundColor = color.range
-          state.table[l][pos[1]].backgroundColor = color.range
-        }
-        const m0 = Math.floor(pos[0] / k), m1 = Math.floor(pos[1] / k)
-        for (let i = m0 * k; i < (m0 + 1) * k; i++) { // ブロック
-          for (let j = m1 * k; j < (m1 + 1) * k; j++) {
-            state.table[i][j].backgroundColor = color.range
+    draw: () => {
+      return ((state, actions) => {
+        if (state.log.length > 0) {
+          if (state.log.length % 100 == 0) console.log(state.log.length);
+          const { pos, val, info } = state.log[0]
+          if (state.previousRecord === null) {
+            changeRangeColor(pos, "lightgray")
+            changeTargetCellColor(pos, val, "lightcoral")
+            updateCell(pos, val, "lightblue") // 最後に呼ぶ
+          } else {
+            const prevPos = state.previousRecord.pos
+            const prevInfo = state.previousRecord.info
+            if (prevInfo === "rollback" || prevInfo === "allowed") {
+              changeRangeColor(prevPos, "white")
+              if (prevInfo === "rollback") {
+                updateCell(prevPos, " ", "white")
+              }
+            }
+            switch (info) {
+              case "confirming":
+                if (prevInfo === "denied") {
+                  changeTargetCellColor(pos, val, "lightgray")
+                }
+                changeRangeColor(pos, "lightgray")
+                changeTargetCellColor(pos, val, "lightcoral")
+                updateCell(pos, val, "lightblue")
+                break
+              case "allowed":
+                changeRangeColor(pos, "lightblue")
+                updateCell(pos, val, "lightskyblue")
+                break
+              case "denied":
+                break
+              case "rollback":
+                changeRangeColor(pos, "lightcoral")
+                updateCell(pos, n, "coral")
+                break
+              case "solved":
+                changeRangeColor(prevPos, "white")
+                break
+            }
           }
+          return ({ table: state.table, log: state.log.slice(1), previousRecord: state.log[0], ID: state.intervalID })
         }
-        state.table[pos[0]][pos[1]].backgroundColor = color.cell
-        return ({ table: state.table })
       })
     },
-    // updateSpeed: (ev) => {
-    //   const co = parseInt(ev.target.value.replace("x", ""), 10)
-    //   return ((state) => {
-    //     return ({ speed: BASE_INTERVAL / co })
-    //   })
-    // }
   }
 
-  const Table = ({ state }) => {
-    return h("table", { border: 1, oncreate: () => trace(state), style: { borderCollapse: "collapse" } },
+  const Table = ({ state, actions }) => {
+    return h("table", { oncreate: () => actions.set(BASE_INTERVAL) },
       h("tbody", {}, state.table.map((row, i) => {
         return h("tr", {}, row.map((cell, j) => {
-          return h("td", { style: { padding: "5px", color: cell.textColor, backgroundColor: cell.backgroundColor } }, cell.val)
+          return h("td", { style: { color: cell.textColor, backgroundColor: cell.backgroundColor, fontStyle: cell.fontStyle } }, cell.val)
         }))
-
       }))
     )
   }
 
   const view = (state, actions) => {
     return h("div", { class: "container" }, [
-      Table({ state }),
-      // TODO ...
-      // h("p", {}, [
-      //   h("form", {},
-      //     h("label", { for: "speed" }, "Speed "),
-      //     h("select", { id: "speed", name: "speed", onchange: (ev) => actions.updateSpeed(ev) }, [
-      //       h("option", { value: "x1", selected: "selected" }, "x1"),
-      //       h("option", { value: "x2" }, "x2"),
-      //       h("option", { value: "x4" }, "x4"),
-      //     ])
-      //   )
-      // ])
+      Table({ state, actions }),
+      h("p", {}, [
+        h("form", {},
+          h("label", { for: "speed" }, "Speed "),
+          h("select", { id: "speed", name: "speed", onchange: (ev) => actions.changeInterval(ev) }, [
+            h("option", { value: "x1" }, "x1"),
+            h("option", { value: "x2", selected: "selected" }, "x2"),
+            h("option", { value: "x4" }, "x4"),
+            h("option", { value: "x8" }, "x8"),
+            h("option", { value: "x16" }, "x16"),
+            h("option", { value: "x32" }, "x32"),
+          ])
+        )
+      ])
     ])
   }
 
   const main = app(state, actions, view, document.getElementById("main"))
-
-  const sleep = (ms) => new Promise((resolve) => setTimeout(() => resolve(), ms))
-
-  const trace = () => {
-    (async () => {
-      let prevPos = [-1, -1], prevInfo = "denied"
-      for (let { pos, val, info } of log) {
-        await sleep(BASE_INTERVAL)
-        switch (info) {
-          case "confirming":
-            if (prevInfo == "allowed") main.changeBackgroundColor({ pos: prevPos, color: { range: "white", cell: "white" } })
-            main.updateCell({ pos, val })
-            main.changeBackgroundColor({ pos, color: { range: "lightgray", cell: "lightblue" } })
-            main.changeTargetCellColor({ pos, val, color: "lightcoral" })
-            break
-          case "allowed":
-            await sleep(BASE_INTERVAL * 5)
-            main.changeBackgroundColor({ pos, color: { range: "lightblue", cell: "lightskyblue" } })
-            await sleep(BASE_INTERVAL * 5)
-            break
-          case "denied":
-            await sleep(BASE_INTERVAL * 5)
-            main.changeTargetCellColor({ pos, val, color: "lightgray" })
-            if (val == n) {
-              main.changeBackgroundColor({ pos: prevPos, color: { range: "lightcoral", cell: "coral" } })
-              await sleep(BASE_INTERVAL * 5)
-            }
-            if (prevInfo == "denied") {
-              main.updateCell({ pos: prevPos, val: 0 })
-              main.changeBackgroundColor({ pos: prevPos, color: { range: "white", cell: "white" } })
-              main.changeBackgroundColor({ pos, color: { range: "lightgray", cell: "lightblue" } })
-            }
-            break
-          case "solved":
-            main.changeBackgroundColor({ pos: prevPos, color: { range: "white", cell: "white" } })
-            break
-        }
-        prevInfo = info
-        if (info != "solved") {
-          prevPos = pos.slice()
-        }
-      }
-    })()
-  }
 }
